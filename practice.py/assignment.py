0e383bf455fbdde9ce552ab4480b2e5dc4563fcc
shivanihousey24 <shivanichouksey24@gmail.com>
2026-06-28 23:23:27 +0530

    add list topic

--- practice.py/assignment.py
+++ practice.py/assignment.py
@@ -4,13 +4,13 @@ prints the sentence "Yes - Spathiphyllum is the best plant ever!"
 to the screen if the inputted string is "Spathiphyllum" (upper-case)prints "No, I want a big Spathiphyllum!" 
 if the inputted string is "spathiphyllum" (lower-case)prints "Spathiphyllum! Not [input]!" otherwise. Note: [input] is the string taken as input.'''
 # solve:-
-# plants_name=input("enter a plants name : ")
-# if plants_name=="Spathiphyllum":
-#     print("Yes - Spathiphyllum is the best plant ever!" )
-# elif plants_name=="spathiphyllum":
-#     print("No, I want a big Spathiphyllum!" )
-# else:
-#     print("Spathiphyllum NOT",plants_name)
+plants_name=input("enter a plants name : ")
+if plants_name=="Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!" )
+elif plants_name=="spathiphyllum":
+    print("No, I want a big Spathiphyllum!" )
+else:
+    print("Spathiphyllum NOT",plants_name)
 ''' Quesytion number 2  
 Once upon a time there was a land a land of milk and honey, inhabited by happy and prosperous people. The people paid taxes, of course – their happiness had limits. The most important tax, called the Personal Income Tax (PIT for short) had to be paid once a year, and was evaluated using the following rule:
 if the citizen's income was not higher than 85,528 thalers, 
@@ -22,15 +22,15 @@ it should print the calculated tax, rounded to full thalers. There's a function 
 this happy country never returned any money to its citizens. If the calculated tax was less than zero, 
 it would only mean no tax at all (the tax was equal to zero). Take this into consideration during your calculations.'''
 # solve:-
-# income=float(input("enter a income number :"))
-# if income<=85528:
-#     tax=(income*0.18)-556.02
-# else:
-#     tax=14839.02+(0.32(income-85528))
-#     if tax<0:
-#          tax=0
-# tax=round(tax)
-# print(tax)
+income=float(input("enter a income number :"))
+if income<=85528:
+    tax=(income*0.18)-556.02
+else:
+    tax=14839.02+(0.32(income-85528))
+    if tax<0:
+         tax=0
+tax=round(tax)
+print(tax)
 '''Question number 3
 As you surely know, due to some astronomical reasons, years may be leap or common. 
 The former are 366 days long, while the latter are 365 days long.
@@ -44,12 +44,12 @@ and needs to be completed with the instructions implementing the test we've just
 The code should output one of two possible messages, which are Leap year or Common year, depending on the value entered.
 It would be good to verify if the entered year falls into the Gregorian era, and output a warning otherwise: Not within the Gregorian calendar period.'''
 # solve:-
-# blocks=int(input("enter your blocks :"))
-# counter=0
-# while(blocks-counter>0):
-#     counter+=1
-#     blocks=blocks-counter
-#     print(f'height of the pyramid :{counter}')
+blocks=int(input("enter your blocks :"))
+counter=0
+while(blocks-counter>0):
+    counter+=1
+    blocks=blocks-counter
+    print(f'height of the pyramid :{counter}')
 '''Practice Question number 4 
 The Beatles were one of the most popular music groups of the 1960s, and the best-selling band in history. Some people consider them to be the most influential act of the rock era. Indeed, they were included in Time magazine's compilation of the 20th Century's 100 most influential people.
 The band underwent many line-up changes, culminating in 1962 with the line-up of John Lennon, Paul McCartney, George Harrison, and Richard Starkey (better known as Ringo Starr).
